758cd53ce9cd912ab5133e3a46bc1938c85d3389
Almanac <ruoyw0@gmail.com>
2021-04-30 00:37:54 -0700

    Got basics of multiple scrolling bars working. That's enough for the day. Going to resume tomorrow

--- ClientApp/src/components/Donations/donations.jsx
+++ ClientApp/src/components/Donations/donations.jsx
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
 
 class Donations extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          donations : [],
+          sortingOwn: "all",
+          filterNameOther: ""
+        }
+    }
+
+    generateValues() {
+        let values = [];
+        for (let i = 0; i < 100; i++) {
+            values.push(<div key={i} style={{fontSize: 100}}>Some Large Words</div>);
+        }
+        return values;
+    }
+
     render() {
-        return (
-            <div>
-                <div className="btn btn-primary" onClick={() => this.props.navCallback("main", "")}>Back to Home Page</div>
-                <div>
-                    Page for creating and editing a comic!
+        return(
+            <div className="row">
+                <div className="col-6" style={{overflowY : 'scroll', height: window.innerHeight}}>
+                    {this.generateValues()}
+                </div>
+                <div className="col-6" style={{overflowY : 'scroll', height: window.innerHeight}}>
+                    {this.generateValues()}
                 </div>
             </div>
         );
+    }
+
+    fetchDonations() {
+      let donations = [];
+      for (let i = 0; i < 11; i++) {
+        donations.push({artist : "Person" + i, customer : "Patron" + i, amount : 2 * i + 10, comment : "Love your work!"});
+      }
+      let newState = Object.assign({}, this.state);
+      newState.donations = donations;
+      this.setState(newState);
+    }
+
+    cancelDonation() {
+
     }
 }
 
--- ClientApp/src/components/Workstation/workstation.jsx
+++ ClientApp/src/components/Workstation/workstation.jsx
@@ -4,7 +4,6 @@ class Workstation extends Component {
     render() {
         return (
             <div>
-                <div className="btn btn-primary" onClick={() => this.props.navCallback("main", "")}>Back to Home Page</div>
                 <div>
                     Page for creating and editing a comic!
                 </div>
