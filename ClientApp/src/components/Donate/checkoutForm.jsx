c81465c35de6ff4613963be32010416193516c4e
Almanac <ruoyw0@gmail.com>
2021-05-01 02:16:54 -0700

    Making progress with Stripes API. Almost there

--- ClientApp/src/components/Donate/checkoutForm.jsx
+++ ClientApp/src/components/Donate/checkoutForm.jsx
@@ -1,48 +1,49 @@
 import React from "react";
-import { ElementsConsumer, CardElement } from "@stripe/react-stripe-js";
+import {CardElement, useStripe, useElements} from '@stripe/react-stripe-js';
+import './donate.css';
 
-import CardSection from "./cardSection";
+// minimal example from react.stripes.js documentation
+const CheckoutForm = ({author, amount}) => {
+  const stripe = useStripe();
+  const elements = useElements();
 
-class CheckoutForm extends React.Component {
-  handleSubmit = async event => {
+  const handleSubmit = async (event) => {
+    // Block native form submission.
     event.preventDefault();
 
-    const { stripe, elements } = this.props;
     if (!stripe || !elements) {
+      // Stripe.js has not loaded yet. Make sure to disable
+      // form submission until Stripe.js has loaded.
       return;
     }
 
-    const card = elements.getElement(CardElement);
-    const result = await stripe.createToken(card);
-    if (result.error) {
-      console.log(result.error.message);
+    // Get a reference to a mounted CardElement. Elements knows how
+    // to find your CardElement because there can only ever be one of
+    // each type of element.
+    const cardElement = elements.getElement(CardElement);
+
+    // Use your card Element with other Stripe.js APIs
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
+      type: 'card',
+      card: cardElement,
+    });
+
+    if (error) {
+      console.log('[error]', error);
     } else {
-      console.log(result.token);
+      console.log('[PaymentMethod]', paymentMethod);
     }
   };
 
-  render() {
-    return (
-      <div>
-        <div>
-          <h3>Apple MacBook Pro</h3>
-          <h4>$999</h4>
-        </div>
-        <form onSubmit={this.handleSubmit}>
-          <CardSection />
-          <button disabled={!this.props.stripe} className="btn-primary">Pay Now</button>
-        </form>
-      </div>
-    );
-  }
-}
-
-export default function InjectedCheckoutForm() {
   return (
-    <ElementsConsumer>
-      {({ stripe, elements }) => (
-        <CheckoutForm stripe={stripe} elements={elements} />
-      )}
-    </ElementsConsumer>
+    <form onSubmit={handleSubmit}>
+      <div>{`Donation of $${amount} to ${author}`}</div>
+      <CardElement />
+      <button type="submit" disabled={!stripe} className="btn btn-primary">
+        Confirm Payment
+      </button>
+    </form>
   );
-}
+};
+
+export default CheckoutForm;
--- ClientApp/src/components/Donate/donate.jsx
+++ ClientApp/src/components/Donate/donate.jsx
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./checkoutForm";
+import './donate.css';
 
 const stripePromise = loadStripe("pk_test_51IYBRaIA0hLVSbphflOwHb6VZqwN4at3SyOb9QtTK7i8FwXeBp7OKETjTskQsYDEAZxAuQqRzbynz0ZOjWk5U6sB00h30v7vc4");
 class Donate extends Component {
 
     constructor(props) {
         super(props);
-      }
+
+        this.state = {
+          amount : 1.0,
+          status : "settingAmount"
+        }
+    }
     render() {
         return (
             <div>
@@ -17,7 +23,7 @@ class Donate extends Component {
               <div>
                 <div>
                   <Elements stripe={stripePromise}>
-                    <CheckoutForm />
+                    <CheckoutForm author={this.props.author} amount={this.state.amount}/>
                   </Elements>
                 </div>
               </div>
